9f04b19b129be6f461e075ef4326acdb0c5e4b96
Vafakos Charalampos <xarisvafakos@gmail.com>
2025-04-01 18:23:38 +0300

    feat: Added the routes to the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
 );
